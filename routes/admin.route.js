c6bc7657ed33b69b6e5d6f46ab57660394eb2b67
Vera Kippel <veki2400@student.miun.se>
2026-01-18 14:10:10 +0100

    autetiserar token i PUT-route

--- routes/admin.route.js
+++ routes/admin.route.js
@@ -135,6 +135,7 @@ module.exports = (server) => {
                         abortEarly: false
                     }
                 },
+                auth: 'jwt',
             }
         },
         {
